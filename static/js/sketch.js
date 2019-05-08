65d5c2530da97fca5661cccbcfcb618fc2b84d53
darkwyvren <williamchang435@gmail.com>
2019-05-09 01:26:18 +1000

    implemented hashed password system

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -13,42 +13,75 @@ var sketch = function( sk ) {
         var w = sk.windowWidth;
         var h = sk.windowHeight;
         sk.tick++;
+        sk.updateGameStateTrans();
 
-
-
-        sk.background(0);
-        sk.push();
-        sk.fill(255);
-        sk.textAlign(sk.CENTER);
-        sk.applyFont("phantom");
-        sk.textSize(64);
-        sk.text("DC",w/2,h/2);
-        sk.applyFont("frozito");
-        sk.textSize(24);
-        sk.text("quest",w/2,h/2+30);
-        
-        sk.pop();
-        sk.stroke(255);
-        sk.noFill();
-        //console.log(sk.checkLoading());
-        sk.arc(w/2,h/2-15,200,200,0,sk.PI*2.0*sk.checkLoading());
-        if(sk.loaded()){
-            sk.noStroke();
-            sk.loadTextFade+=(1.0-sk.loadTextFade)*0.1;
-            sk.fill(255*sk.loadTextFade);
+        if(sk.gamestate===0){
+            sk.background(0);
+            sk.push();
+            sk.fill(255);
             sk.textAlign(sk.CENTER);
-            sk.applyFont("unseen");
-            sk.textSize(18);
-            sk.text("Welcome "+sk.playerName,w/2,h/2+110);
+            sk.applyFont("phantom");
+            sk.textSize(64);
+            sk.text("DC",w/2,h/2);
+            sk.applyFont("frozito");
+            sk.textSize(24);
+            sk.text("quest",w/2,h/2+30);
 
+            sk.pop();
+            sk.stroke(255);
+            sk.noFill();
+            //console.log(sk.checkLoading());
+            sk.loadBarAni+=(sk.checkLoading()-sk.loadBarAni)*0.2;
+            sk.arc(w/2,h/2-15,200,200,0,sk.PI*2.0*sk.loadBarAni);
+            if(sk.loaded()){
+                sk.noStroke();
+                sk.loadTextFade+=(1.0-sk.loadTextFade)*0.1;
+                sk.fill(255*sk.loadTextFade);
+                sk.textAlign(sk.CENTER);
+                sk.applyFont("unseen");
+                sk.textSize(18);
+                sk.text("Welcome "+sk.playerName,w/2,h/2+110);
+                if(sk.loadTextFade>0.99999){
+                    sk.changeGameState(1);
+                }
+            }
+        }else if(sk.gamestate===1){
+            sk.fill(255);
+            sk.rect(0, 0, w, h);
         }
+        if(sk.gamestateAni!=1) {
+            sk.fill(0, 255 - 255 * sk.abs(sk.gamestateAni));
+            sk.noStroke();
+            sk.rect(0, 0, w, h);
+        }
+
     };
     /// ANIMATIONS
     sk.loadTextFade = 0;
+    sk.loadBarAni = 0;
+    sk.gamestateAni = 1;
 
 
     /// MAJOR VARIABLES --------------------------------------
     sk.gamestate = 0;
+    sk.requestGamestate = 0;
+
+    sk.changeGameState = function(state){
+        if(sk.requestGamestate==state){
+            return;
+        }
+        sk.requestGamestate = state;
+        sk.gamestateAni=-1;
+
+    };
+    sk.updateGameStateTrans = function(){
+        if(sk.gamestateAni>0){
+            sk.gamestate=sk.requestGamestate;
+        }
+        sk.gamestateAni = sk.constrain(sk.gamestateAni+0.07,-1,1);
+    };
+
+
 
     //RESOURCE RETRIEVAL -------------------------------------------
     sk.httpGetMap = new Map();
@@ -76,34 +109,34 @@ var sketch = function( sk ) {
 
     sk.isUndef = function(s){
         return typeof s==="undefined";
-    }
+    };
   
     sk.httpPost= function (theUrl,value){
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", theUrl, true);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify({
-			value: value
-	}));
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", theUrl, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+                value: value
+        }));
 
-	return xhr.responseText;
+        return xhr.responseText;
     };
     sk.httpGet = function (theUrl)
-    {		
-        var xmlHttp = new XMLHttpRequest();
+    {
+        let xmlHttp = new XMLHttpRequest();
         xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
         xmlHttp.send( null );
         return xmlHttp.responseText;
     };
     sk.getFromServer = function (getFromServer,keyForMap){
-            httpGet("/"+getFromServer).then( (message) => {
+            sk.httpGet("/"+getFromServer).then( (message) => {
                     sk.httpGetMap.set(keyForMap,message);
             }).catch( (message) => {
             console.log(message);
             })
     };
     sk.sendToServer = function (sendToServer,value,keyForMap){
-            httpPost("/"+sendToServer,value).then( (message) => {
+            sk.httpPost("/"+sendToServer,value).then( (message) => {
                     sk.httpGetMap.set(keyForMap,message);
             }).catch( (message) => {
             console.log(message);
--- wow.py
+++ wow.py
@@ -1,11 +1,15 @@
 import random
 import os
 import time
+import redis
+import hashlib
 
 from flask import Flask, abort, request, send_file, render_template
 from flask_cors import CORS
 import json
 
+r = redis.from_url("redis://h:p5d77d98513cd400d85c1ecf76a4e92b6435e49fe7fa3258082dbe20d9d0d43b5@ec2-34-193-52-1.compute-1.amazonaws.com:22319")
+#r = redis.from_url(os.environ.get("REDIS_URL"))
 
 # from distutils.core import setup
 
@@ -34,8 +38,24 @@ class User:
 userlist = []
 
 
+def getFromDB(key):
+    return r.get(key).decode("utf-8")
+
+
+def existInDB(key):
+    return r.exists(key)
+
+
+def sendToDB(key, value):
+    r.set(key, value)
+
+
+def removeFromDB(key):
+    r.delete(key)
+
+
 # to send images go: return send_file(filename, mimetype='image/Insert_image_format_here')
-def doAction(action):
+def do_action(action):
     print(action)
     # ADD SHIT HERE
     return "Player does " + action
@@ -51,32 +71,63 @@ def getTestImage():
 
 
 @app.route('/font/<fontname>')
-def getFont(fontname):
-    time.sleep(random.randint(0, 10) / 10.0)  ## TEST LATENCY, REMOVE LATER
+def get_font(fontname):
+    # time.sleep(random.randint(0, 10) / 10.0)  ## TEST LATENCY, REMOVE LATER
     return send_file("static/font/" + fontname + ".otf", mimetype='font/otf')
 
 
-@app.route('/')
-def loginPage():
+@app.route('/registerUSR', methods=['POST'])
+def register():
+    username = request.form['username']
+    if len(username) < 3:
+        return render_template('register.html', errormsg="username above 2 characters plz")
+    password = request.form['password']
+    if len(password) < 7:
+        return render_template('register.html', errormsg="password above 6 characters plz")
+    password2 = request.form['passwordrepeat']
+    token = request.form['token']
+    if password != password2:
+        return render_template('register.html', errormsg="passwords dont match")
+
+    if not existInDB("DCt0k3n" + token):
+        return render_template('register.html', errormsg="invalid token :O")
+
+    passhash = hashlib.md5(bytes(request.form['password'], 'utf-8')).hexdigest()
+    sendToDB(username, passhash)
+    # removeFromDB("DCt0k3n"+token)  # not yet! :)
     return render_template('login.html', errormsg=" ")
+
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html', errormsg=" ")
+
+
+@app.route('/')
+def login_page():
+    return render_template('register.html', errormsg=" ")
 
 
 # Login a user, (username only, implement password later)
 @app.route('/login', methods=['POST'])
-def userLogin():
+def user_login():
     # user is already logged in
-    for user in userlist:
-        if user.name == request.form['username']:
-            return render_template('login.html', errormsg="user is active!")
-    # add user to active users
-    newuser = User(request.form['username'])
+    usrname = request.form['username']
+    if usrname != "devtest":  # bypass user
+        if not existInDB(usrname):
+            return render_template('login.html', errormsg="user doesnt exist")
+
+        if not getFromDB(usrname) == hashlib.md5(bytes(request.form['password'], 'utf-8')).hexdigest():
+            return render_template('login.html', errormsg="incorrect password")
+        # add user to active users
+    newuser = User(usrname)
     userlist.append(newuser)
     print("USER:", newuser.name, newuser.accessToken)
-    return render_template('game.html', name=request.form['username'], token=newuser.accessToken)
+    return render_template('game.html', name=usrname, token=newuser.accessToken)
 
 
 @app.route('/userDidSomething', methods=['POST'])
-def userAction():
+def user_action():
     print(request.json, request.data)
     # split the data by spaces
     # user action is vadlidated by a token.
@@ -91,9 +142,14 @@ def userAction():
             confirm = True
 
     if confirm:
-        return doAction(splitData[2])
+        return do_action(splitData[2])
 
     return "error 400"
+
+
+@app.route('/devuserlist')
+def getusrs():
+    return userlist
 
 
 # or wherever your SSL keys are
