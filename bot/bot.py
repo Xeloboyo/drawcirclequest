b92422bc1a58e80cb5e45518f15c8dc07c45550a
Harley Whales <whales.harley@gmail.com>
2019-06-02 19:41:26 +1000

    Moved discord bot to bot folder, to be removed from repo later

---
+++ bot/bot.py
@@ -0,0 +1,63 @@
+import random
+import time
+
+import discord
+import wow
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+TOKEN = "NTc2MDA2NTg1NzYwMDg4MDg1.XNQOFg.TmubKCPbHjYaXPPAUXHpM9cLAtk"
+client = discord.Client()
+commandprefix = "$$"
+
+client = Bot(command_prefix=commandprefix)
+
+def randomSTR(size):
+    accessToken = ""
+    for i in range(0, size):
+        accessToken += chr(random.randint(ord('a'), ord('z')))
+    return accessToken
+
+@client.command()
+async def suicide(ctx):
+    await client.logout()
+
+
+@client.command()
+async def genkey(ctx):
+    token = randomSTR(10)
+    wow.sendToDB("DCt0k3n" + token, time.time())
+    wow.r.expire("DCt0k3n" + token,3600*6)
+    await ctx.send("Your new token is:"+token+"\n    Login at https://draw-circle-quest.herokuapp.com/register")
+
+@client.command()
+async def getUserCount(ctx):
+    await ctx.send("No. Users Online:"+str(len(wow.getFromDB("USERS").split())))
+
+
+@client.command()
+async def getRedisUsage(ctx):
+    await ctx.send(str(wow.r.execute_command("MEMORY STATS")))
+
+@client.command()
+async def getUsers(ctx):
+    await ctx.send("Users Online:"+wow.getFromDB("USERS"))
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Message not recognised, please try again.")
+        return
+    await ctx.send("Error:"+str(error))
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+#client.run(TOKEN)
+client.run(TOKEN)
+
